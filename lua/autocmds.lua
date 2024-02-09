55628f06a3c4a084d80017b03abd02db1cb6aa62
Venu Poruri <vmporuri@gmail.com>
2024-02-08 21:43:40 -0800

    feat: change indentation based on file type

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,15 +1,24 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
+local yank_group = augroup("HighlightYank", {})
+local indent_group = augroup("SmallIndent", {})
 
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 100,
-        })
-    end,
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 100,
+		})
+	end,
 })
 
+autocmd("FileType", {
+	group = indent_group,
+	pattern = { "c", "html" },
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+	end,
+})
