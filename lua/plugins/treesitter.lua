ef8db137687825fb61ece50198355800c48fccf6
Venu Poruri <vmporuri@gmail.com>
2024-02-18 01:34:20 -0800

    feat: add tree-sitter-textobjects plugin + keymaps

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,43 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = { 'windwp/nvim-ts-autotag' },
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+        "nvim-treesitter/nvim-treesitter-textobjects"
+    },
     build = ":TSUpdate",
     config = function()
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query",
-                "elixir", "heex", "javascript", "html", "python", "markdown",
-                "markdown_inline" },
+            ensure_installed = {
+                "c", "lua", "vim", "vimdoc", "javascript", "html", "css",
+                "python", "markdown", "markdown_inline",
+            },
             sync_install = false,
             highlight = { enable = true, disable = { "css" }, },
             indent = { enable = true },
             autotag = {
                 enable = true,
                 filetypes = {
-                    'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx',
-                    'jsx', 'rescript',
-                    'css', 'lua', 'xml', 'php', 'markdown'
+                    "html", "javascript", "typescript", "javascriptreact",
+                    "typescriptreact", "svelte", "vue", "tsx",
+                    "jsx", "rescript",
+                    "css", "lua", "xml", "php", "markdown",
                 },
             },
+            textobjects = {
+                select = {
+                    enable = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["a?"] = "@conditional.outer",
+                        ["i?"] = "@conditional.inner",
+                    },
+                }
+            }
         })
     end
 }
