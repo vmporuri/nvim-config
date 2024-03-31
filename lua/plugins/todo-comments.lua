251cc33784d30028247bbe6d97104b75552754fe
Venu Poruri <vmporuri@gmail.com>
2024-03-31 00:34:33 -0500

    feat: add todo-comment highlighting + nav

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	lazy = false,
+	opts = {},
+	keys = {
+		{
+			"]t",
+			function()
+				require("todo-comments").jump_next()
+			end,
+		},
+		{
+			"[t",
+			function()
+				require("todo-comments").jump_prev()
+			end,
+		},
+	},
+}
