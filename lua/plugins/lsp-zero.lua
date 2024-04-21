01c7c2b2f8d8df72bac1031590d0aade590626df
Venu Poruri <vmporuri@gmail.com>
2024-04-21 03:54:46 -0700

    refactor: change cmp remaps

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -95,10 +95,10 @@ return {
 		})
 
 		local ls = require("luasnip")
-		vim.keymap.set({ "i", "s" }, "<Tab>", function()
+		vim.keymap.set({ "i", "s" }, "<C-L>", function()
 			ls.jump(1)
 		end, { silent = true })
-		vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+		vim.keymap.set({ "i", "s" }, "<C-J>", function()
 			ls.jump(-1)
 		end, { silent = true })
 	end,
