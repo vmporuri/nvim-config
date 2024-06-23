36dbccbbcda4297a707912d804ff566f0ff0f586
Venu Poruri <vmporuri@gmail.com>
2024-06-23 03:41:33 -0500

    feat: add docker lsp and linter

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -14,6 +14,7 @@ return {
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			go = { "golangcilint" },
+			dockerfile = { "hadolint" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
