2142f2786b7fcc480ac180d0c280b686013998ee
Venu Poruri <vmporuri@gmail.com>
2024-06-30 03:02:16 -0500

    fix: overlapping leap keymaps

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,6 +1,8 @@
 return {
 	"ggandor/leap.nvim",
 	config = function()
-		require("leap").create_default_mappings()
+		vim.keymap.set({ "n", "x", "o" }, "<Leader>s", "<Plug>(leap-forward)")
+		vim.keymap.set({ "n", "x", "o" }, "<Leader>S", "<Plug>(leap-backward)")
+		vim.keymap.set({ "n", "x", "o" }, "<Leader>gs", "<Plug>(leap-from-window)")
 	end,
 }
